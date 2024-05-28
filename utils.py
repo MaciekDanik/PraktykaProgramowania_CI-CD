e204caabae9cc2e903662eb89b8337c6f77c6b84
MaciekDanik <maciekdanik@gmail.com>
2024-05-28 12:14:48 +0200

    comit 5

--- utils.py
+++ utils.py
@@ -1,15 +1,27 @@
+"""
+dodawanie
+"""
 def add(a: int, b: int) -> int:
     return a + b
 
 
+"""
+odejmowanie
+"""
 def subtract(a: int, b: int) -> int:
     return a - b
 
 
+"""
+mnożenie
+"""
 def multiply(a: int, b: int) -> int:
     return a * b
 
 
+"""
+dzielenie
+"""
 def divide(a: int, b: int) -> int:
     return a / b
 #komentarz
